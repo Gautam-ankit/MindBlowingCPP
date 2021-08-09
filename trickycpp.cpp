d3e82797b96795db8b46311ba71741c59ec9ad66
Gautam-ankit <ankit.gautam@sait.ac.in>
2021-08-10 00:06:08 +0530

    Accessing private attribute using virtual function & Inheritance

--- trickycpp.cpp
+++ trickycpp.cpp
@@ -1,1 +1,31 @@
-#include<bits/stdc++.h>
+#include<iostream>
+
+using namespace std;
+
+class parent{
+    private:
+    protected:
+    public:
+        virtual void print() {}; 
+};
+
+class child : public parent{
+    private:
+        string code;
+        void print(){
+            cout<<"code is "<<code;
+        }
+    public:
+        child(string c){
+            code = c;
+        }
+};
+int main() {
+	child c("mycode");
+	
+	//c.print();
+	
+	parent *p = &c;
+	p->print();
+	return 0;
+}
